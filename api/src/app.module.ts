9b21a96a18e91f00957c7f02df76c47530f064cb
Heiler Nova <heilernov@outlook.com>
2024-05-28 18:21:15 -0500

    Add(API): creamos le módulo de los servicios comunes

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ModelsModule } from './models/models.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ModelsModule],
+  imports: [ModelsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
