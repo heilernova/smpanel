1b935040fd704edf1c6fe1da73e979a0566cf36a
Heiler Nova <heilernov@outlook.com>
2024-05-28 18:21:39 -0500

    Add(API): creamos le módulo de los controladores

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ModelsModule } from './models/models.module';
 import { CommonModule } from './common/common.module';
+import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
-  imports: [ModelsModule, CommonModule],
+  imports: [ModelsModule, CommonModule, ControllersModule],
   controllers: [AppController],
   providers: [AppService],
 })
