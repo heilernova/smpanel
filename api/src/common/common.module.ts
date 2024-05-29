48ea3133657e4348d90a533e0ef2b5ceb04bda46
Heiler Nova <heilernova@outlook.com>
2024-05-28 19:13:51 -0500

    Add(API): servicio de PostgreSQL

--- api/src/common/common.module.ts
+++ api/src/common/common.module.ts
@@ -1,5 +1,13 @@
 import { Global, Module } from '@nestjs/common';
+import { ConnectionDbService } from './connection-db';
+
+const services = [
+    ConnectionDbService
+]
 
 @Global()
-@Module({})
+@Module({
+    providers: services,
+    exports: services
+})
 export class CommonModule {}
